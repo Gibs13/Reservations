fca44e61cb988cc935dbca9fbfc6c50264468f57
Gibs13 <Gsgwlol@gmail.com>
2017-06-26 13:43:44 +0200

    test

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let ApiAiApp = require('actions-on-google').ApiAiApp;
 let express = require('express');
 let bodyParser = require('body-parser');
 let horraires = require('./horraires.js');
+let restaurants = require('./restaurants.js');
 
 let app = express();
 app.use(bodyParser.json({type: 'application/json'}));
@@ -37,16 +38,110 @@ app.post('/', function (req, res) {
         }
     }
 
-    function reserve (assistant) {
+    function select(array) {
+        /*if (assistant.data.state == WELCOME_STATE) {
+            
+            assistant.data.state = RESERVE_STATE;
+            assistant.ask("Voici une liste de vos restaurants préférés proches : " );
+        } else if (assistant.data.state == RESERVE_STATE) {
+            assistant.data.restaurant = restaurants[Math.floor(Math.random()*restaurants.length)];
+        }*/
+        return array[Math.floor(Math.random()*array.length)];
+    }
+
+    function get_City (assistant) {
+
+    }
+
+    function get_Restaurant (assistant) {
+        if (assistant.getArgument('restaurant') == null) {
+            assistant.data.state = CHOOSE_R_STATE;
+            return true;
+        }
+        assistant.data.restaurant = assistant.getArgument('restaurant');
+    }
+
+    function get_Date (assistant) {
+        if (assistant.getArgument('date') == null) {
+            assistant.data.state = CHOOSE_D_STATE;
+            return true;
+        }
+        assistant.data.date = assistant.getArgument('date');
+    }
+
+    function get_Name (assistant) {
+        if (assistant.getArgument('name') == null) {
+            assistant.data.state = CHOOSE_N_STATE;
+            return true;
+        }
+        assistant.data.name = assistant.getArgument('name');
+
+    }
+
+    function confirmation () {
+
+    }
+
+    function reserver (assistant) {
+
+        /*if (assistant.data.state != RESERVE_STATE && assistant.data.state != WELCOME_STATE) {
+            assistant.ask("are you sure ?");
+            return;
+        }*/
 
         goodDate(assistant);
-        assistant.data.city = assistant.getArgument('city')
-
+        assistant.data.city = assistant.getArgument('city');
+        assistant.data.restaurant 
 
 
 
 
         assistant.ask(assistant.data.city);
+    }
+
+    // intents
+
+    function start (assistant) {
+        assistant.data.state = WELCOME_STATE;
+        assistant.ask("Bienvenue, souhaitez vous reservez un restaurant ?")
+    }
+    
+    function choose (assistant) {
+        if (assistant.data.state == WELCOME_STATE) {
+
+        }
+    }
+
+    function reserve (assistant) {
+        if (assistant.data.state != WELCOME_STATE) {
+            confirmation();
+        }
+        if (get_Restaurant(assistant)){
+            assistant.ask("Quel restaurant ?")
+            return;
+        }
+        if (get_Date(assistant)){
+            assistant.ask("A quelle date ?")
+            return;
+        }
+        let restaurant = assistant.data.restaurant;
+        let date = assistant.data.date;
+        assistant.ask("A" + date + "au " + restaurant)
+    }
+
+    function yes (assistant) {
+        if (assistant.data.state == WELCOME_STATE) {
+            reserve();
+        }
+
+    }
+
+    function no (assistant) {
+        if (assistant.data.state == WELCOME_STATE) {
+            quit(assistant);
+            return;
+        }
+
     }
 
     function quit (assistant) {
